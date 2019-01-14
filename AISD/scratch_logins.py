b2fea4ccbc6742c4af1f6ee5093617a3b252f904
jeansalac <salac@canon.cs.uchicago.edu>
2019-01-14 11:31:44 -0600

    Added more students for AISD Morris' classes

--- AISD/scratch_logins.py
+++ AISD/scratch_logins.py
@@ -22,9 +22,9 @@ def main():
 		header = "Student First Name,Student Last Name,Username,Password"
 		print>>teacherCSV, header
 		
-		#Create 30 accounts
+		#Create 50 accounts
 		i = 1
-		for i in range(1,31):
+		for i in range(1,51):
 			username = line[0]+line[1]+str(i)
 			password = str(random.randint(0,99))+words[random.randint(0,len(words)-1)]+str(random.randint(0,99))
 			scratchRow = username+","+password
