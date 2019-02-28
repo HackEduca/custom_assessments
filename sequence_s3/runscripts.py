940a05ba3f96965fa44683cc3657a0d9ca670d59
Qi Jin <qijin@Qis-MacBook-Pro.local>
2019-02-28 01:00:02 -0600

    Commit

--- sequence_s3/runscripts.py
+++ sequence_s3/runscripts.py
@@ -7,10 +7,10 @@ import pandas as pd
 # import crop_img
 
 scratch_projects = [
-"https://scratch.mit.edu/studios/5752419/"]
+]
 
 project_names = [
-"GP_3rd"]
+]
 
 def find_users(filename):
 
@@ -30,44 +30,43 @@ def find_users(filename):
 # "https://scratch.mit.edu/studios/5831059/", "SES_Class_4C_(300-23*)",
 # "https://scratch.mit.edu/studios/5831066/", "SES_Class_5A_(311-23)",
 # "https://scratch.mit.edu/studios/5831076/", "SES_Class_5B_(312-23)",
+# "https://scratch.mit.edu/studios/5752419/", "GP_3rd"
 
 lsp = len(scratch_projects)
 
 for i in range(lsp):
 
 	project = scratch_projects[i]
-	# call(["python", "sequenceQnGen.py", project])
+	call(["python", "sequenceQnGen.py", project])
 
-	# call(["python3", "parse_json.py"])
+	call(["python3", "parse_json.py"])
 
-	# img_directory = "img_files/"
+	img_directory = "img_files/"
 
-	# directory = ""
-	# files = os.listdir(".")
-	# if '.DS_Store' in files:
-	# 	files.remove('.DS_Store')
+	directory = ""
+	files = os.listdir(".")
+	if '.DS_Store' in files:
+		files.remove('.DS_Store')
 
-	# c = 0
-	# filedir = {}
-	# for filename in files:
-	# 	filedir[c] = filename
-	# 	if filename[-3:] == ".js":
-	# 		call(["node", directory + filename])
-	# 		old_name = img_directory + "scratchblocks.png"
-	# 		new_name = img_directory + filename[:-3] + ".png"
-	# 		if "script" not in filename:
-	# 			new_name = img_directory + filename[:-3] + "_script0" + ".png"
-	# 		try:
-	# 			os.rename(old_name, new_name)
-	# 			call(["python3", "crop_img.py", new_name, img_directory])
-	# 		except:
-	# 			print(filename)
-	# 		c += 1
+	c = 0
+	filedir = {}
+	for filename in files:
+		filedir[c] = filename
+		if filename[-3:] == ".js":
+			call(["node", directory + filename])
+			old_name = img_directory + "scratchblocks.png"
+			new_name = img_directory + filename[:-3] + ".png"
+			if "script" not in filename:
+				new_name = img_directory + filename[:-3] + "_script0" + ".png"
+			try:
+				os.rename(old_name, new_name)
+				call(["python3", "crop_img.py", new_name, img_directory])
+			except:
+				print(filename)
+			c += 1
 
-	# writedir = open(project_names[i] + "_filedirectory.json", "w")
-	# writedir.write(json.dumps(filedir))
-
-	# break
+	writedir = open(project_names[i] + "_filedirectory.json", "w")
+	writedir.write(json.dumps(filedir))
 
 	call(["python3", "maketex.py"])
 
@@ -174,5 +173,3 @@ for i in range(lsp):
 	student_directory = open(project_names[i] + "_directory.txt", "w")
 	student_directory.write(output)
 	student_directory.close()
-
-	break
